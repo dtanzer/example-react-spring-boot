bdcb929b288c314f123fbddac596f5d1c411932b
David Tanzer <david@davidtanzer.net>
2016-08-22 07:31:17 +0200

    Flow and Mocha are working now in IDEA and Gradle

---
+++ web-frontend/src/test/interfaces/mocha.js
@@ -0,0 +1,7 @@
+declare class describe {
+	static (description: string, spec: () => void): void;
+}
+
+declare class it {
+	static (description: string, spec: () => void): void;
+}
--- web-frontend/src/test/javascript/environment.spec.js
+++ web-frontend/src/test/javascript/environment.spec.js
@@ -1,3 +1,4 @@
+//@flow
 import { expect } from 'chai';
 
 describe('the environment', () => {
